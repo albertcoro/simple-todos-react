f773cacfeac333d05d38fba3a28155db23a34873
Coro <albertcorominasmariscot@gmail.com>
2022-09-27 17:16:26 +0200

    Step 7 in tutorial

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,23 +1,101 @@
-import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import React, { Fragment, useState } from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task.jsx';
+import { TaskForm } from './TaskForm.jsx';
+import { LoginForm } from './LoginForm.jsx';
 
-const tasks = [
-  {_id: 1, text: 'First Task'},
-  {_id: 2, text: 'Second Task'},
-  {_id: 3, text: 'Third Task'},
-];
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+};
 
-    <ul>
-      { tasks.map(task => <Task key={ task._id } task={ task }/>) }
-    </ul>
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
 
-    <Hello/>
-    <Info/>
-  </div>
-);
+
+
+export const App = () => {
+  const user = useTracker(() => Meteor.user());
+  const [hideCompleted, setHideCompleted] = useState(false);
+
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+
+  const userFilter = user ? { userId: user._id } : {};
+
+  const pendingOnlyFilter = { ...hideCompletedFilter, ...userFilter };
+
+  const tasks = useTracker(() => {
+    if (!user) {
+      return [];
+    }
+
+    return TasksCollection.find(
+      hideCompleted ? pendingOnlyFilter : userFilter,
+      {
+        sort: { createdAt: -1 },
+      }
+    ).fetch();
+  });
+
+  const pendingTasksCount = useTracker(() => {
+    if (!user) {
+      return 0;
+    }
+
+    return TasksCollection.find(pendingOnlyFilter).count();
+  });
+
+  const pendingTasksTitle = `${
+    pendingTasksCount ? ` (${pendingTasksCount})` : ''
+  }`;
+
+  const logout = () => Meteor.logout();
+  
+  return (
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>🔥 My First App 🔥
+              {pendingTasksTitle}
+            </h1>
+          </div>
+        </div>
+      </header>
+
+      <div className="main">
+        {user ? (
+          <Fragment>
+            <div className="user" onClick={logout}>
+              {user.username} 🚪
+            </div>
+            <TaskForm user={user} />
+
+            <div className="filter">
+              <button onClick={() => setHideCompleted(!hideCompleted)}>
+                {hideCompleted ? 'Show All' : 'Hide Completed'}
+              </button>
+            </div>
+
+            <ul className="tasks">
+              {tasks.map(task => (
+                <Task
+                  key={task._id}
+                  task={task}
+                  onCheckboxClick={toggleChecked}
+                  onDeleteClick={deleteTask}
+                />
+              ))}
+            </ul>
+          </Fragment>
+        ) : (
+          <LoginForm />
+        )}
+      </div>
+    </div>
+  );
+};
--- server/main.js
+++ server/main.js
@@ -1,5 +1,41 @@
 import { Meteor } from 'meteor/meteor';
 import { LinksCollection } from '/imports/api/links';
+import { TasksCollection } from '/imports/api/TasksCollection';
+
+
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
+const insertTask = taskText => TasksCollection.insert({ text: taskText });
+
+Meteor.startup(() => {
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
+  const user = Accounts.findUserByUsername(SEED_USERNAME);
+
+  if (TasksCollection.find().count() === 0) {
+    [
+      'First Task',
+      'Second Task',
+      'Third Task',
+      'Fourth Task',
+      'Fifth Task',
+      'Sixth Task',
+      'Seventh Task',
+      '8 Task',
+      '9 Task',
+      '10 Task',
+      '11 Task',
+      '12 Task',
+      '13 Task',
+      '14 Task'
+    ].forEach(insertTask)
+  }
+});
 
 function insertLink({ title, url }) {
   LinksCollection.insert({title, url, createdAt: new Date()});
